0a636110b6a5e7b864e37b6984de4a6f74f7ea46
Josue Huaman <64330912+Josuehu@users.noreply.github.com>
2022-08-27 12:53:14 -0500

    2

--- sketch.js
+++ sketch.js
@@ -21,14 +21,20 @@ function setup() {
     qt.show();
 }
 
-function draw() {
-    background(0);
-    if (mouseIsPressed) {
-        for (let i = 0; i < 1; i++) {
-            let m = new Point(mouseX + random(-5, 5), mouseY + random(-5, 5));
-            qt.insert(m)
-        }
+function draw (){
+    background (0);
+    qt.show ();
+
+    stroke (0 ,255 ,0) ;
+    rectMode ( CENTER );
+    let range = new Rectangle ( mouseX ,mouseY ,50 ,50)
+    rect ( range.x , range.y , range.w *2, range.h *2) ;
+    let points = [];
+    qt.query (range , points );
+
+    for (let p of points ){
+      strokeWeight (4) ;
+      point (p.x , p.y );
     }
-    background(0);
-    qt.show();
+
 }
