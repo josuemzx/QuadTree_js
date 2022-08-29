2bf2c2b3968a5ee6c1eb74fd5ed6506234918789
Josue Huaman <64330912+Josuehu@users.noreply.github.com>
2022-08-29 00:53:06 -0500

    QuadTree de tamaño 500x500

--- quadtree.js
+++ quadtree.js
@@ -8,20 +8,18 @@ class Point {
 
 class Rectangle {
     constructor(x, y, w, h) {
-        this.x = x; // center
+        this.x = x;
         this.y = y;
-        this.w = w; // half width
-        this.h = h; // half height
+        this.w = w;
+        this.h = h;
     }
-
-// verifica si este objeto contiene un objeto Punto
+    
     contains(point) {
-
-    }
-
-// verifica si este objeto se intersecta con otro objeto Rectangle
+       return (point.x >= this.x - this.w && point.x <= this.x + this.w && point.y >= this.y - this.h && point.y <= this.y + this.h );
+     }
+      
     intersects(range) {
-
+       return !(range.x - range.w > this.x + this.w || range.x + range.w < this.x - this.w || range.y - range.h > this.y + this.h || range.y + range.h < this.y - this.h);
     }
 }
 
@@ -33,36 +31,66 @@ class QuadTree {
         this.divided = false;
     }
 
-// divide el quadtree en 4 quadtrees
     subdivide() {
-// Algoritmo
-// 1: Crear 4 hijos: qt_northeast , qt_northwest , qt_southeast ,
-
-        qt_southwest
-
-// 2: Asignar los QuadTree creados a cada hijo
-// this.northeast = qt_northeast;
-// this.northwest = qt_northwest;
-// this.southeast = qt_southeast;
-// this.southwest = qt_southwest;
-
-// 3.- Hacer: this.divided <- true
+        let x = this.boundary.x;
+        let y = this.boundary.y;
+        let w = this.boundary.w;
+        let h = this.boundary.h;
+        let qt_northeast = new Rectangle(x + w / 2, y - h / 2, w / 2, h / 2);
+        let qt_northwest = new Rectangle(x - w / 2, y - h / 2, w / 2, h / 2);
+        let qt_southeast = new Rectangle(x + w / 2, y + h / 2, w / 2, h / 2);
+        let qt_southwest = new Rectangle(x - w / 2, y + h / 2, w / 2, h / 2);
+        this.northeast = new QuadTree(qt_northeast, this.capacity);
+        this.northwest = new QuadTree(qt_northwest, this.capacity);
+        this.southeast = new QuadTree(qt_southeast, this.capacity);
+        this.southwest = new QuadTree(qt_southwest, this.capacity);
+        this.divided = true;
     }
 
     insert(point) {
-// Algoritmo
-// 1: Si el punto no esta en los limites ( boundary ) del quadtree Return
 
-// 2: Si ( this.points.length ) < ( this.capacity ),
-// 2.1 Insertamos en el vector this.points
-// Sino
-// 2.2 Dividimos si aun no ha sido dividido
-// 2.3 Insertamos recursivamente en los 4 hijos.
-// this.northeast.insert ( point );
-// this.northwest.insert ( point );
-// this.southeast.insert ( point );
-// this.southwest.insert ( point );
+        if (!this.boundary.contains(point)){
+            return false;
+        }
 
+        if (this.points.length < this.capacity) {
+            this.points.push(point);
+            return true;
+        } 
+        else {
+            if(!this.divided){                
+                this.subdivide();                
+            }
+            return (
+                this.northeast.insert(point) ||
+                this.northwest.insert(point) ||
+                this.southeast.insert(point) ||
+                this.southwest.insert(point)
+              );
+        }
+    }
+
+    query(range, found){    
+        if(!found){
+            found=[];
+        }
+        if(!this.boundary.intersects(range)){
+            return found;
+        }
+        else{
+          for(let point of this.points){
+            if(range.contains(point)){
+                found.push(point)
+            }
+          }
+          if(this.divided){            
+            this.northeast.query(range,found);
+            this.northwest.query(range,found);
+            this.southeast.query(range,found);
+            this.southwest.query(range,found);
+          } 
+          return found;
+        }
     }
 
     show() {
@@ -83,5 +111,4 @@ class QuadTree {
             point(p.x, p.y);
         }
     }
-  
 }
--- sketch.js
+++ sketch.js
@@ -1,17 +1,10 @@
-let qt;
-let count = 0;
-
 function setup() {
-    createCanvas(400, 400);
-
-// centre point and half of width and height
-    let boundary = new Rectangle(200, 200, 200, 200);
-
-// each leave just could have 4 elements
+    let quadCanvas = createCanvas(500, 500);
+    quadCanvas.parent("QuadTreeCanvas");
+    let boundary = new Rectangle(250, 250, 250, 250);
     qt = new QuadTree(boundary, 4);
 
     console.log(qt);
-
     for (let i = 0; i < 3; i++) {
         let p = new Point(Math.random() * 400, Math.random() * 400);
         qt.insert(p);
@@ -20,21 +13,3 @@ function setup() {
     background(0);
     qt.show();
 }
-
-function draw (){
-    background (0);
-    qt.show ();
-
-    stroke (0 ,255 ,0) ;
-    rectMode ( CENTER );
-    let range = new Rectangle ( mouseX ,mouseY ,50 ,50)
-    rect ( range.x , range.y , range.w *2, range.h *2) ;
-    let points = [];
-    qt.query (range , points );
-
-    for (let p of points ){
-      strokeWeight (4) ;
-      point (p.x , p.y );
-    }
-}
-    
